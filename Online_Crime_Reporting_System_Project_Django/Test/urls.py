b760810cf71d4e0dd3843a642cb5e4ca497bbb88
jadhavhpranav1121 <jadhavhpranav1121@gmail.com>
2023-02-12 00:31:12 +0530

    changes

--- Online_Crime_Reporting_System_Project_Django/Test/urls.py
+++ Online_Crime_Reporting_System_Project_Django/Test/urls.py
@@ -9,7 +9,8 @@ from django.contrib import admin
 from blockchain.views import *  
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    # path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('', TemplateView.as_view(template_name='contact.html'), name='contact'),
     path('admin/', admin.site.urls),
     path('accounts/', include(('accounts.urls', 'accounts'), namespace='accounts')),
     path('complaints/', include(('complaints.urls', 'complaints'), namespace='complaints')),
